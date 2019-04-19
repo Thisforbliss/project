ec2a062c381d7e881b247780851f6c8bf23f27eb
Malcolm Leake <echelon1089@gmail.com>
2019-04-19 00:23:07 +0000

    still confused

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,11 @@
 #require "project/version"
+require_relative './project/version'
+require_relative './project/cli'
+
+#require 'pry'
+#require 'nokogiri'
+
+
 
 module Project
   class Error < StandardError; end
