6e139d556d3c0a742d4327fb4a7693ee808c57b4
Malcolm Leake <echelon1089@gmail.com>
2019-04-21 20:30:35 +0000

    establsih names

--- lib/project/scraper.rb
+++ lib/project/scraper.rb
@@ -4,12 +4,16 @@ class Scraper
     html = open("https://www.onthisday.com/people/basketball-players")
     doc = Nokogiri::HTML(html)
     doc.css("li.photo-list__item").each do |player|
+     players = player.text.split.(" ")
+     first_name =  players[2]
+     last_name = players[3]
     binding.pry
     
     
-    Player.new(first_name, last_name)
+    #Player.new(first_name, last_name)
     
     end
   end
   
 end
+
