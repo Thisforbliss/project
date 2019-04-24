cbc722871106f13546abce38bdbaaa45d0461d18
Malcolm Leake <echelon1089@gmail.com>
2019-04-24 07:48:37 +0000

    w

--- lib/project/scraper.rb
+++ lib/project/scraper.rb
@@ -1,24 +1,20 @@
 class Scraper
   
   def scrape_page
-    html = open("https://www.onthisday.com/people/basketball-players")
-    doc = Nokogiri::HTML(html)
-    doc.css("ul.photo-list.photo-list--full-width li.photo-list__item").each do |player|
-      name = player.css("a").children.text
-      new_player = Player.new(name)
-      new_player.url=  "https://www.onthisday.com"player.css("a").attr("href").value
-      scrape_player(player)
+    html = open("http://www.espn.com/nba/team/roster/_/name/gs")
+    website = Nokogiri::HTML(html)
+    section = website.css("table.Table2__table__wrapper")
+    #players = 
+      
+    #   new_player = Player.new(name)
+    
+       
       binding.pry
-    end
+    #end
   end 
     
     
-  def self.scrape_player(player)
-    html = open("https://www.onthisday.com"#{new_player.url})
-    doc = Nokogiri::HTML(html)
-    doc.css("wrapper").each do |player| 
-    binding.pry
-    end
-  end
+
+
 end
 
