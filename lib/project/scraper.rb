7f07a3f75fbe9e6d5e83dc2e822d2e6ddc9c3037
Malcolm Leake <echelon1089@gmail.com>
2019-04-24 17:38:34 +0000

    m

--- lib/project/scraper.rb
+++ lib/project/scraper.rb
@@ -1,9 +1,9 @@
 class Scraper
   
   def scrape_page
-    html = open("http://www.espn.com/nba/team/roster/_/name/gs")
+    html = open("http://www.espn.com/nba/player/bio/_/id/3975/stephen-curry")
     website = Nokogiri::HTML(html)
-    section = website.css("table.Table2__table__wrapper")
+    section = website.css("div.Wrapper.Card__Content")
     #players = 
       
     #   new_player = Player.new(name)
@@ -18,3 +18,5 @@ class Scraper
 
 end
 
+section.first.text
+=> "TeamGolden State WarriorsPosition Point GuardHT/WT6' 3\", 190 lbsDOB3/14/1988 (31)CollegeDavidsonDraft Info2009: Rd 1, Pk 7 (GS)StatusActiveExperience9th Season"
