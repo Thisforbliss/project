25c6d252bcaa5c962e804a5825ada6086479caf9
Malcolm Leake <echelon1089@gmail.com>
2019-04-24 05:20:30 +0000

    url update

--- lib/project/scraper.rb
+++ lib/project/scraper.rb
@@ -6,15 +6,15 @@ class Scraper
     doc.css("ul.photo-list.photo-list--full-width li.photo-list__item").each do |player|
       name = player.css("a").children.text
       new_player = Player.new(name)
-      new_player.url=  player.css("a").attr("href").value
+      new_player.url=  "https://www.onthisday.com"player.css("a").attr("href").value
       scrape_player(player)
       binding.pry
     end
   end 
     
     
-  def scrape_player(player)
-    html = open("https://www.onthisday.com/people/basketball-players")
+  def self.scrape_player(player)
+    html = open("https://www.onthisday.com"#{new_player.url})
     doc = Nokogiri::HTML(html)
     doc.css("wrapper").each do |player| 
     binding.pry
