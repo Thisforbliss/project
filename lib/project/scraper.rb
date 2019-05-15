7b7182668a6f652674a28fbbe5ff63a66275374b
Malcolm Leake <echelon1089@gmail.com>
2019-05-15 20:26:41 +0000

    progess

--- lib/project/scraper.rb
+++ lib/project/scraper.rb
@@ -1,11 +1,11 @@
-require 'pry'
+require 'pry'.
 class Scraper
   
   def scrape_page
     html = open("http://www.espn.com/nba/team/roster/_/name/gs")
     website = Nokogiri::HTML(html)
-     website.css("tr.Table2__tr").each do |player|
-    
+     website.css("tr.Table2__tr.Table2__tr--lg").each do |player|
+      full_name = player.css("figure").attr("title").value
      
      binding.pry
     end
