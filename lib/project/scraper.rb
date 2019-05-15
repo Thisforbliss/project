d4aa6e090f1b2f733530f5fec566afcdecc86749
Malcolm Leake <echelon1089@gmail.com>
2019-05-15 19:51:03 +0000

    progress

--- lib/project/scraper.rb
+++ lib/project/scraper.rb
@@ -4,30 +4,12 @@ class Scraper
   def scrape_page
     html = open("http://www.espn.com/nba/team/roster/_/name/gs")
     website = Nokogiri::HTML(html)
-    stats = website.css("tr.Table2__tr")
+     website.css("tr.Table2__tr").each do |player|
     
-    # name = "Stephen Curry"
-    # steph = Player.new(name)
-    # team = stats.first.children[0].text
-    # position = stats.first.children[1].text
-    # height_weight = stats.first.children[2].text
-    # dob = stats.first.children[3].text
-    # college = stats.first.children[3].text
      
-      
-    
- 
-      
-   
-    
-       
-      binding.pry
-    #end
+     binding.pry
+    end
   end 
-    
-    
-
-
 end
 
-#section.first.children[0].text
+
