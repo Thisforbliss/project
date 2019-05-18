a87126e55dace7faf898b8cbdb55b53bbbe7a7cd
Malcolm Leake <echelon1089@gmail.com>
2019-05-18 19:09:04 +0000

    building cli

--- lib/project/cli.rb
+++ lib/project/cli.rb
@@ -1,21 +1,48 @@
-class CLI 
+class CLI
+  
+  attr_accessor :player, :continue
+  
+  def intialize
+   @continue = true 
+  end
+  
+  def call 
+    start 
+    while @continue 
+    menu
+    select_player
+    menu if @continue && @player != nil
+    end
+  end
   
   def start
-    puts "Welcome to the Basketball Hall of Fame where you could learn about your favorite basketaball players!!!"
-    puts "Choose your player buy entering the number listed."
-    Scraper.new.scrape_page
-    menu
-    number = gets.chomp!
-   end
+   puts "Welcome to the Golden State Roster 'The Home Of The Splash Brothers'"
+  end
+  
+  def goodbye
+    puts "See you next time for any players updates!"
+  end
   
   def menu
+    #Scraper.new.scrape_page
+    Player.all.clear
+    Scraper.scrape_page
     Player.all.each.with_index(1) do |player,index|
       puts "#{index}. #{player.name}"
     end
   end
   
-  
-
-
+  def select_player
+  input = ""
+  puts "\n Choose any player you wish by the number or type 'exit' when finished."
+  input = gets.chomp.strip
+    if input = "exit"
+      @continue = false
+    elsif input.to_i.between?(1, Player.all.length)
+      @player = Player.all[input.to_i - 1]
+    else
+      puts "Error, Choose any player you wish by the number or type 'exit' when finished.  "
+    end
+  end
 
 end
