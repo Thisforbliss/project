bcb234b08cbb3790a27ca7e3830649b40878cc49
Malcolm Leake <echelon1089@gmail.com>
2020-01-13 21:47:05 +0000

    updated scraping and refactored code

--- lib/project/scraper.rb
+++ lib/project/scraper.rb
@@ -6,18 +6,17 @@ class Scraper
   def self.scrape_page
     html = open("http://www.espn.com/nba/team/roster/_/name/gs")
     website = Nokogiri::HTML(html)
+    players = website.css(".Table__TBODY tr")
 
-     website.css("tr.Table2__tr.Table2__tr--lg").each do |player|
-      name = player.css("figure").attr("title").value
-      new_player = Player.new(name)
-      new_player.position = player.css(".Table2__td")[2].text
-      new_player.age = player.css(".Table2__td")[3].text
-      new_player.height = player.css(".Table2__td")[4].text
-      new_player.weight = player.css(".Table2__td")[5].text
-      new_player.college = player.css(".Table2__td")[6].text
-      new_player.salary = player.css(".Table2__td")[7].text
-
+    players.each do |player|
+       name = player.css(".Table__TD")[1].text
+       new_player = Player.new(name)
+       new_player.position = player.css(".Table__TD")[2].text
+       new_player.age = player.css(".Table__TD")[3].text
+       new_player.height = player.css(".Table__TD")[4].text
+       new_player.weight = player.css(".Table__TD")[5].text
+       new_player.college = player.css(".Table__TD")[6].text
+       new_player.salary = player.css(".Table__TD")[7].text
     end
   end
-
 end
